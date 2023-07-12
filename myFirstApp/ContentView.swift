4e6ae601f7e36828ddd61618f9d99e04af62cf4d
Z W-F <zoewfjuliette@gmail.com>
2023-07-12 14:34:13 -0700

    Initial Commit

---
+++ myFirstApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  myFirstApp
+//
+//  Created by Scholar on 7/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text ("a verrrrryyyyy cute animal")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(Color.purple)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
